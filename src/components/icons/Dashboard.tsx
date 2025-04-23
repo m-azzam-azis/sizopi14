468bc4c595ce52f4f611c10ace0ea01c7eac8142
m.azzam <m.azzam.azis@gmail.com>
2025-04-23 15:45:05 +0700

    chore: init shadcn, fix html tag

--- src/components/icons/Dashboard.tsx
+++ src/components/icons/Dashboard.tsx
@@ -14,26 +14,26 @@ export const Dashboard: React.FC<IconProps> = ({
       className={`${size} ${className}`}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        clipRule="evenodd"
+        clipRule="evenodd"
         d="M2 4C2 2.89543 2.89543 2 4 2H9C10.1046 2 11 2.89543 11 4V11C11 12.1046 10.1046 13 9 13H4C2.89543 13 2 12.1046 2 11V4ZM9 4H4V11H9V4Z"
         fill="#253F52"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        clipRule="evenodd"
+        clipRule="evenodd"
         d="M13 4C13 2.89543 13.8954 2 15 2H20C21.1046 2 22 2.89543 22 4V7C22 8.10457 21.1046 9 20 9H15C13.8954 9 13 8.10457 13 7V4ZM20 4H15V7H20V4Z"
         fill="#253F52"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        clipRule="evenodd"
+        clipRule="evenodd"
         d="M13 13C13 11.8954 13.8954 11 15 11H20C21.1046 11 22 11.8954 22 13V20C22 21.1046 21.1046 22 20 22H15C13.8954 22 13 21.1046 13 20V13ZM20 13H15V20H20V13Z"
         fill="#253F52"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        clipRule="evenodd"
+        clipRule="evenodd"
         d="M2 17C2 15.8954 2.89543 15 4 15H9C10.1046 15 11 15.8954 11 17V20C11 21.1046 10.1046 22 9 22H4C2.89543 22 2 21.1046 2 20V17ZM9 17H4V20H9V17Z"
         fill="#253F52"
       />
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,31 +1,39 @@
-import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
-import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "flex flex-row items-center justify-center gap-2 whitespace-nowrap cursor-pointer rounded-[12px] transition-all disabled:pointer-events-none  [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 md:[&_svg:not([class*='size-'])]:size-6 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
-        primary:
-          "bg-orange-500 max-md:text-[12px] max-md:leading-[16px] font-semibold md:text-s7 font-outfit hover:bg-orange-600 pressed:bg-orange-700 disabled:bg-orange-800 disabled:text-orange-400 text-white",
+        default:
+          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+        outline:
+          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
-          "bg-orange-100 max-md:text-[12px] max-md:leading-[16px] font-semibold text-s8 md:text-s7 font-outfit hover:bg-orange-200 pressed:bg-orange-300 disabled:bg-orange-400 text-orange-500 disabled:text-orange-600",
+          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
-          "bg-transparent border-[1px] border-orange-500 max-md:text-[12px] max-md:leading-[16px] font-semibold md:text-s7 font-outfit hover:bg-orange-050 pressed:bg-orange-100 disabled:bg-transparent disabled:text-orange-100 disabled:border-orange-100 text-orange-500",
+          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+        link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "px-4 md:px-5 py-3 md:py-4 h-10 md:h-14",
+        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        icon: "size-9",
       },
     },
     defaultVariants: {
-      variant: "primary",
+      variant: "default",
       size: "default",
     },
   }
-);
+)
 
 function Button({
   className,
@@ -35,9 +43,9 @@ function Button({
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
-    asChild?: boolean;
+    asChild?: boolean
   }) {
-  const Comp = asChild ? Slot : "button";
+  const Comp = asChild ? Slot : "button"
 
   return (
     <Comp
@@ -45,7 +53,7 @@ function Button({
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
-  );
+  )
 }
 
-export { Button, buttonVariants };
+export { Button, buttonVariants }
