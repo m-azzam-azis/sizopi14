a9f4ac25bd127aebbab9af06e802f017dfa9a357
sayyidtrq <sayyidthariq02@gmail.com>
2025-04-27 20:13:57 +0700

    feat: add pakan,rekam-medis,jadwal pemeriksaan pages

---
+++ src/app/jadwal-pemeriksaan/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { JadwalPemeriksaanModule } from "@/modules/JadwalPemeriksaanModule";
+
+const PemeriksaanPage = () => {
+  return <JadwalPemeriksaanModule />;
+};
+
+export default PemeriksaanPage;
---
+++ src/app/pakan/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { PemberianPakanModule } from "@/modules/PemberianPakanModule";
+
+const index = () => {
+  return <PemberianPakanModule />;
+};
+
+export default index;
