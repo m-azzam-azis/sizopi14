9da8a9d359d23d5b6b4620c89c44fa18363a8995
m.azzam <m.azzam.azis@gmail.com>
2025-05-28 13:37:16 +0700

    chore: uncommented post in ds.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -15,7 +15,7 @@ if (isDebug) {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    // port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
     ssl: {
       rejectUnauthorized: false,
     },
